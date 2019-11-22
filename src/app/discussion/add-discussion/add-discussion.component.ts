4b6e631de0b2f42bf42d94bb7342b57fd339a035
ekrej <ekrej@hotmail.nl>
2019-11-22 11:22:34 +0100

    added routingand base object structuse

---
+++ src/app/discussion/add-discussion/add-discussion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-discussion',
+  templateUrl: './add-discussion.component.html',
+  styleUrls: ['./add-discussion.component.scss']
+})
+export class AddDiscussionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/discussion/discussion-detail/discussion-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-discussion-detail',
+  templateUrl: './discussion-detail.component.html',
+  styleUrls: ['./discussion-detail.component.scss']
+})
+export class DiscussionDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
