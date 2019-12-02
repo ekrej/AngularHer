920d364f305f42aad7551577e628d443807cd969
ekrej <ekrej@hotmail.nl>
2019-12-02 14:59:43 +0100

    comments can be eddited

--- src/app/discussion/edit-comment/edit-comment.component.ts
+++ src/app/discussion/edit-comment/edit-comment.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CommentService } from 'src/services/comment.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-comment',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-comment.component.scss']
 })
 export class EditCommentComponent implements OnInit {
-
-  constructor() { }
+  @Input() oldContent: string;
+  @Input() id: string;
+  @Input() discussion: string;
+  commentForm: FormGroup;
+  constructor(private commentService: CommentService) { }
 
   ngOnInit() {
+    this.commentForm = new FormGroup({
+      content: new FormControl('', [
+        Validators.required
+      ]),
+    });
+  }
+
+  // loadForm() {
+  //   this.showIt = !this.showIt;
+  // }
+
+  get content() {
+    return this.commentForm.get('content')
+  }
+
+  onSubmit() {
+      this.commentService.updateComment(this.content.value, this.id, this.discussion)
   }
 
 }
--- src/services/comment.service.ts
+++ src/services/comment.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { stringify } from 'querystring';
 
 @Injectable()
 export class CommentService {
@@ -24,12 +25,23 @@ export class CommentService {
             })
     }
 
-    addCommentOnComment(username: string, content: string, id: string, discussion) {
+    addCommentOnComment(username: string, content: string, id: string, discussion: string) {
 
         this.http.post<any>(`https://angularherapi.herokuapp.com/api/comment/${id}`, {
             username,
             content,
             discussion
+        })
+            .subscribe((response) => {
+                this.router.navigateByUrl('/discussion', { skipLocationChange: true }).then(() => {
+                    this.router.navigate([`/discussion/${discussion}`]);
+                });
+            })
+    }
+
+    updateComment(content: string, id: string, discussion: string){
+        this.http.put<any>(`https://angularherapi.herokuapp.com/api/comment/${id}`, {
+            content
         })
             .subscribe((response) => {
                 this.router.navigateByUrl('/discussion', { skipLocationChange: true }).then(() => {
